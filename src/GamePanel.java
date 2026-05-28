d593338567fa931c35937743acd0b6bc9801cecc
Lev Khyzhnychenko <3014673k@student.gla.ac.uk>
2026-05-28 13:21:39 +0100

    Changed the folder structure

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,13 @@
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.swing.JPanel;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
 
 }
